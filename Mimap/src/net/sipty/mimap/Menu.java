11a43bb7a729d3c4085d4a281fc82ad1530469a9
Sipty <choko43@gmail.com>
2013-09-26 01:42:05 +0300

    revert

--- Mimap/src/net/sipty/mimap/Menu.java
+++ Mimap/src/net/sipty/mimap/Menu.java
@@ -4,16 +4,12 @@ public class Menu {
 	
 	// declarations
 	private static Boolean showMenu=false,
-						   showOptions=false,
-						   showQuit=false,
-						   showRestart=false;
+						   showOptions=false;
 	//private final int menuLeftX=GameScreen.getMenuLeftX()+1, menuRightX=GameScreen.getMenuLeftX()+59, menuTopY=GameScreen.getMenuTopY()+45;
-	private final static int leftMenuX=50,	// menu coords 
-							 rightMenuX=GameScreen.getMenuRightX()+5,
+	private final static int menuX=50,	// menu coords 
 							 menuY=700,
-							 restartY=menuY-20,	// restart coords	
-							 optionsY=menuY-40,	// options coords
-							 quitY=menuY-60;	// quit coords
+							 optionsX=GameScreen.getMenuRightX()+5,	// options coords
+							 optionsY=menuY-40;
 	
 	// constructor
 	public Menu() {
@@ -21,31 +17,20 @@ public class Menu {
 	
 	public static void draw() {
 		// menu button
-		GameScreen.game.font.draw(GameScreen.game.batch, "Menu", leftMenuX, menuY);
+		GameScreen.game.font.draw(GameScreen.game.batch, "Menu", menuX, menuY);
 		
 		// menu drop-down
 		if(showMenu) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Restart", leftMenuX, restartY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Options", leftMenuX, optionsY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Quit", 	leftMenuX, quitY);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Restart", menuX, menuY-20);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Options", menuX, menuY-40);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Quit", menuX, menuY-60);
 		}
 
-		// restart
-		if(showRestart) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Are you sure?", rightMenuX, restartY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "No  /  Yes", rightMenuX, restartY-20);
-		}
 		// options menu
 		if(showOptions) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 1", rightMenuX, optionsY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 2", rightMenuX, optionsY-20);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 3", rightMenuX, optionsY-40);
-		}
-		
-		// quit confirmation
-		if(showQuit) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Are you sure?", rightMenuX, quitY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "No  /  Yes", 	  rightMenuX, quitY-20);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Option 1", optionsX, optionsY);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Option 2", optionsX, optionsY-20);
+			GameScreen.game.font.draw(GameScreen.game.batch, "Option 3", optionsX, optionsY-40);
 		}
 	}
 
@@ -65,22 +50,6 @@ public class Menu {
 
 	public static void setShowOptions(Boolean showOptions) {
 		Menu.showOptions = showOptions;
-	}
-
-	public static Boolean getShowQuit() {
-		return showQuit;
-	}
-
-	public static void setShowQuit(Boolean showQuit) {
-		Menu.showQuit = showQuit;
-	}
-
-	public static Boolean getShowRestart() {
-		return showRestart;
-	}
-
-	public static void setShowRestart(Boolean showRestart) {
-		Menu.showRestart = showRestart;
 	}
 
 	
