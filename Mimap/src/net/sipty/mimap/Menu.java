eff6e017fde1495e2311a9582801146c75ee947b
Sipty <choko43@gmail.com>
2013-10-02 22:54:28 +0100

    Transfered all base values for menu to Menu. It also adjusted to the namechange of GameScreen to InHouseScreen.

--- Mimap/src/net/sipty/mimap/Menu.java
+++ Mimap/src/net/sipty/mimap/Menu.java
@@ -9,8 +9,9 @@ public class Menu {
 						   showRestart=false;
 	//private final int menuLeftX=GameScreen.getMenuLeftX()+1, menuRightX=GameScreen.getMenuLeftX()+59, menuTopY=GameScreen.getMenuTopY()+45;
 	private final static int leftMenuX=50,	// menu coords 
-							 rightMenuX=GameScreen.getMenuRightX()+5,
-							 menuY=700,
+							// rightMenuX=GameScreen.getMenuRightX()+5,
+							 rightMenuX=110,
+							 menuY=718,
 							 restartY=menuY-20,	// restart coords	
 							 optionsY=menuY-40,	// options coords
 							 quitY=menuY-60;	// quit coords
@@ -21,38 +22,52 @@ public class Menu {
 	
 	public static void draw() {
 		// menu button
-		GameScreen.game.font.draw(GameScreen.game.batch, "Menu", leftMenuX, menuY);
+		InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Menu", leftMenuX, menuY);
 		
 		// menu drop-down
 		if(showMenu) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Restart", leftMenuX, restartY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Options", leftMenuX, optionsY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Quit", 	leftMenuX, quitY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Restart", leftMenuX, restartY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Options", leftMenuX, optionsY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Quit", 	leftMenuX, quitY);
 		}
 
 		// restart
 		if(showRestart) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Are you sure?", rightMenuX, restartY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "No  /  Yes", rightMenuX, restartY-20);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Are you sure?", rightMenuX, restartY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "No  /  Yes", rightMenuX, restartY-20);
 		}
 		// options menu
 		if(showOptions) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 1", rightMenuX, optionsY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 2", rightMenuX, optionsY-20);
-			GameScreen.game.font.draw(GameScreen.game.batch, "Option 3", rightMenuX, optionsY-40);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Option 1", rightMenuX, optionsY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Option 2", rightMenuX, optionsY-20);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Option 3", rightMenuX, optionsY-40);
 		}
 		
 		// quit confirmation
 		if(showQuit) {
-			GameScreen.game.font.draw(GameScreen.game.batch, "Are you sure?", rightMenuX, quitY);
-			GameScreen.game.font.draw(GameScreen.game.batch, "No  /  Yes", 	  rightMenuX, quitY-20);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "Are you sure?", rightMenuX, quitY);
+			InHouseScreen.game.font.draw(InHouseScreen.game.batch, "No  /  Yes", 	  rightMenuX, quitY-20);
 		}
 	}
 
 	// setters & getters
 	
+	
+	
 	public static void setShowMenu(Boolean showMenu) {
 		Menu.showMenu = showMenu;
+	}
+
+	public static int getMenuY() {
+		return menuY;
+	}
+
+	public static int getLeftMenuX() {
+		return leftMenuX;
+	}
+
+	public static int getRightMenuX() {
+		return rightMenuX;
 	}
 
 	public static Boolean getShowMenu() {
